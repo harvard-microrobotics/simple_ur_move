7bb8942e2a0ed9a05e913523a188a158cab42421
Clark Teeple <cbteeple@g.harvard.edu>
2022-03-23 18:26:03 -0400

    added joint trajectory handler and updated features

--- src/simple_ur_move/joint_trajectory_handler.py
+++ src/simple_ur_move/joint_trajectory_handler.py
@@ -5,7 +5,8 @@ import rospy
 import rospkg
 import actionlib
 
-from cartesian_control_msgs.msg import FollowCartesianTrajectoryAction, FollowCartesianTrajectoryGoal, CartesianTrajectory, CartesianTrajectoryPoint, CartesianTolerance, CartesianPosture
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal, JointTolerance
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Vector3, Twist, Accel, Pose, Point, Quaternion
 from tf2_msgs.msg import TFMessage
 from sensor_msgs.msg import JointState
@@ -25,9 +26,9 @@ import numpy as np
 
 filepath_config = os.path.join(rospkg.RosPack().get_path('simple_ur_move'), 'config')
 
-class CartesianTrajectoryHandler():
+class JointTrajectoryHandler():
     """
-    The cartesian trajectory interface
+    The joint trajectory interface
 
     Parameters
     ----------
@@ -56,11 +57,11 @@ class CartesianTrajectoryHandler():
         self.controller_handler.set_controller(self.controller_to_use)
 
         # Make an action client for trajectories
-        action_name = self.robot_name+'/'+self.controller_to_use+'/follow_cartesian_trajectory'
+        action_name = self.robot_name+'/'+self.controller_to_use+'/follow_joint_trajectory'
 
         self.trajectory_client = actionlib.SimpleActionClient(
                     action_name,
-                    FollowCartesianTrajectoryAction)
+                    FollowJointTrajectoryAction)
         try:
             self.trajectory_client.wait_for_server(rospy.Duration(2.0))
         except rospy.exceptions.ROSException as err:
@@ -121,7 +122,7 @@ class CartesianTrajectoryHandler():
         self.path_tolerance=self.settings.get('path_tolerance',None)
         self.goal_tolerance=self.settings.get('goal_tolerance',None)
         self.goal_time_tolerance=self.settings.get('goal_time_tolerance',None)
-        self.controlled_frame=self.settings.get('controlled_frame',None)
+        self.joint_names=self.settings.get('joint_names',None)
 
 
     def set_initialize_time(self, time):
@@ -143,7 +144,7 @@ class CartesianTrajectoryHandler():
         if speed_factor<=0:
             raise ValueError("Speed_factor time must be strictly greater than 0")
         else:
-            self.speed_factor=speed_factor
+            self.speed_factor=float(speed_factor)
 
 
     def set_trajectory(self, trajectory):
@@ -152,7 +153,7 @@ class CartesianTrajectoryHandler():
 
         Parameters
         ----------
-        trajectory : dict or cartesian_control_msgs/CartesianTrajectory
+        trajectory : dict or trajectory_msgs/JointTrajectory
             Trajectory to parse
 
         Raises
@@ -160,7 +161,7 @@ class CartesianTrajectoryHandler():
         ValueError : If a trajectory of incorrect type is passed
         """
         
-        if not isinstance(trajectory, CartesianTrajectory) and not isinstance(trajectory, dict):
+        if not isinstance(trajectory, JointTrajectory) and not isinstance(trajectory, dict):
             raise ValueError("Joint trajectories must be of type dict or trajectory_msgs/JointTrajectory")
         else:
             self.trajectory = trajectory
@@ -168,7 +169,7 @@ class CartesianTrajectoryHandler():
 
     def pack_trajectory(self, trajectory):
         """
-        Pack a trajectory into a ROS ``CartesianTrajectory``
+        Pack a trajectory into a ROS ``JointTrajectory``
 
         Parameters
         ----------
@@ -177,7 +178,7 @@ class CartesianTrajectoryHandler():
         
         Returns
         -------
-        ros_traj : cartesian_control_msgs/CartesianTrajectory
+        ros_traj : trajectory_msgs/JointTrajectory
             A ROS trajectory
         """
         traj_to_use = copy.deepcopy(trajectory)
@@ -188,30 +189,15 @@ class CartesianTrajectoryHandler():
         for waypoint in traj_to_use:
             if self.debug:
                 print('quaternion: ',waypoint['orientation'])
-            pt = CartesianTrajectoryPoint()
-            pose = Pose()
-            pose.position= Point(x=waypoint['position'][0],
-                                 y=waypoint['position'][1],
-                                 z=waypoint['position'][2],)
-            pose.orientation = Quaternion(x=waypoint['orientation'][0],
-                                          y=waypoint['orientation'][1],
-                                          z=waypoint['orientation'][2],
-                                          w=waypoint['orientation'][3])
-            pt.pose=pose
+            pt = JointTrajectoryPoint()
+            pt.positions = waypoint['positions']
 
-            lin_vel = None
-            ang_vel = None
-            if 'linear_velocity' in waypoint.keys() and 'angular_velocity' in waypoint.keys():
-                lin_vel = waypoint['linear_velocity']
-                ang_vel = waypoint['angular_velocity']
-                pt.twist=Twist(linear=Vector3(x=lin_vel[0], y=lin_vel[1], z=lin_vel[2]),
-                               angular=Vector3(x=ang_vel[0], y=ang_vel[1], z=ang_vel[2]))
-
-            if 'posture' in waypoint.keys():
-                posture= CartesianPosture()
-                posture.posture_joint_values = waypoint['posture']['posture_joint_values']
-                posture.posture_joint_names = waypoint['posture']['posture_joint_names']
-                pt.posture=posture
+            if 'velocities' in waypoint.keys():
+                pt.velocities = waypoint['velocities']
+            if 'accelerations' in waypoint.keys():
+                pt.accelerations = waypoint['accelerations']
+            if 'effort' in waypoint.keys():
+                pt.effort = waypoint['effort']
 
             pt.time_from_start = rospy.Duration(waypoint['time'])
             traj_packed.append(pt)
@@ -220,7 +206,7 @@ class CartesianTrajectoryHandler():
             # Trajectories cannot start at time=0 becasue of how the UR ROS driver is written
             traj_packed[0].time_from_start+=rospy.Duration(0.001)
 
-        traj_ros = CartesianTrajectory(points=traj_packed, controlled_frame=self.controlled_frame)
+        traj_ros = JointTrajectory(points=traj_packed, joint_names=self.joint_names)
         return traj_ros
 
     
@@ -242,50 +228,60 @@ class CartesianTrajectoryHandler():
             Converted trajectory
         """
         if direction == 'to_ros':
-            # Convert mm to m
-            if self.units['position']=='mm':
-                for waypoint in trajectory:
-                    waypoint['position'] = (np.array(waypoint['position'])/1000).tolist()
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = (np.array(waypoint['linear_velocity'])/1000).tolist()
-
             # Convert degrees to radians
             if 'degree' in self.units['orientation']:
+                parts_to_convert=['positions', 'velocities', 'accelerations']
                 for waypoint in trajectory:
-                    waypoint['orientation'] = np.deg2rad(waypoint['orientation']).tolist()
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = np.deg2rad(waypoint['linear_velocity']).tolist()
-
-            # Convert euler angles to quaternians
-            if 'euler' in self.units['orientation']:
-                for waypoint in trajectory:
-                    waypoint['orientation'] = quaternion_from_euler(*waypoint['orientation'])
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = quaternion_from_euler(*waypoint['linear_velocity'])
+                    for part in parts_to_convert:
+                        if part in waypoint.keys():
+                            waypoint['part'] = np.deg2rad(waypoint['part']).tolist()
 
         elif direction == 'from_ros':
-            # Convert mm to m
-            if self.units['position']=='mm':
-                for waypoint in trajectory:
-                    waypoint['position'] = (np.array(waypoint['position'])*1000).tolist()
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = (np.array(waypoint['linear_velocity'])*1000).tolist()
-
-            # Convert quaternion to euler angles
-            if 'euler' in self.units['orientation']:
-                for waypoint in trajectory:
-                    waypoint['orientation'] = euler_from_quaternion(waypoint['orientation'])
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = euler_from_quaternion(waypoint['linear_velocity'])
-
             # Convert radians to degrees
             if 'degree' in self.units['orientation']:
+                parts_to_convert=['positions', 'velocities', 'accelerations']
                 for waypoint in trajectory:
-                    waypoint['orientation'] = np.rad2deg(waypoint['orientation']).tolist()
-                    if waypoint.get('linear_velocity', False):
-                        waypoint['linear_velocity'] = np.rad2deg(waypoint['linear_velocity']).tolist()
+                    for part in parts_to_convert:
+                        if part in waypoint.keys():
+                            waypoint['part'] = np.rad2deg(waypoint['part']).tolist()
 
         return trajectory
+
+
+    def build_goal(self, trajectory):
+        """
+        Build the trajectory goal
+
+        Parameters
+        ----------
+        trajectory : dict or trajectory_msgs/JointTrajectory
+            Trajectory to parse
+        """
+        if trajectory is None:
+            rospy.logerr("Cannot build trajectory goal. It was not defined.")
+            return
+
+        if isinstance(trajectory, JointTrajectory):
+            traj = trajectory
+        else:
+            traj= self.pack_trajectory(trajectory)
+
+        if self.debug:
+            print("")
+            for row in traj.points:
+                print(row.time_from_start.to_sec(), row.positions)
+
+        # Pack the trajectory into a trajectory goal        
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory = traj
+        if self.path_tolerance is not None:
+            goal.path_tolerance = self.path_tolerance
+        if self.goal_tolerance is not None:
+            goal.goal_tolerance = self.goal_tolerance
+        if self.goal_time_tolerance is not None:
+            goal.goal_time_tolerance = self.goal_time_tolerance
+
+        return goal
 
 
     def _run_trajectory(self, trajectory, blocking=True):
@@ -294,7 +290,7 @@ class CartesianTrajectoryHandler():
 
         Parameters
         ----------
-        trajectory : dict or cartesian_control_msgs/CartesianTrajectory
+        trajectory : dict or trajectory_msgs/JointTrajectory
             Trajectory to parse
         blocking : bool
             Whether to wait for the trajectory to finish.
@@ -303,23 +299,11 @@ class CartesianTrajectoryHandler():
             rospy.logerr("Cannot run trajectory. It was not defined.")
             return
 
-        if isinstance(trajectory, CartesianTrajectory):
-            traj = trajectory
-        else:
-            traj= self.pack_trajectory(trajectory)
+        if isinstance(trajectory, FollowJointTrajectoryGoal):
+            goal = trajectory
         
-        if self.debug:
-            print("")
-            for row in traj.points:
-                print(row.time_from_start.to_sec(), row.pose.position.x, row.pose.position.y, row.pose.position.z)
-        goal = FollowCartesianTrajectoryGoal()
-        goal.trajectory = traj
-        if self.path_tolerance is not None:
-            goal.path_tolerance = self.path_tolerance
-        if self.goal_tolerance is not None:
-            goal.goal_tolerance = self.goal_tolerance
-        if self.goal_time_tolerance is not None:
-            goal.goal_time_tolerance = self.goal_time_tolerance
+        else:
+            goal = self.build_goal(trajectory)        
 
         self.trajectory_client.send_goal(goal)
 
@@ -333,30 +317,30 @@ class CartesianTrajectoryHandler():
 
         Parameters
         ----------
-        point : dict or cartesian_control_msgs/CartesianTrajectoryPoint
+        point : dict or trajectory_msgs/JointTrajectoryPoint
             Trajectory point to go to
         """
-        if isinstance(point, CartesianTrajectoryPoint):
+        
+        if isinstance(point, JointTrajectoryPoint):
             start_pt = copy.deepcopy(point)
             start_pt.time_from_start = rospy.Duration(self.initialize_time)
-            traj = CartesianTrajectory(points=[start_pt], controlled_frame=self.controlled_frame)
+            traj = JointTrajectory(points=[start_pt], joint_names=self.joint_names)
         
         else:
             start_pt = copy.deepcopy(point)
             start_pt['time'] = self.initialize_time
             traj = [start_pt]
 
-
         self._run_trajectory(trajectory=traj, blocking=True) 
 
 
     def run_trajectory(self, trajectory=None, blocking=True, perform_init=True):
         """
-        Run the current trajectory
+        Run a trajectory.
 
         Parameters
         ----------
-        trajectory : dict or cartesian_control_msgs/CartesianTrajectory
+        trajectory : dict or trajectory_msgs/JointTrajectory
             Trajectory to run. If excluded, the currently loaded trajectory is run
         blocking : bool
             Whether to wait for the trajectory to finish.
@@ -370,28 +354,39 @@ class CartesianTrajectoryHandler():
         else:
             trajectory = copy.deepcopy(trajectory)
 
-        if isinstance(trajectory, CartesianTrajectory):
+        if isinstance(trajectory, FollowJointTrajectoryGoal):
+            first_pt = trajectory.trajectory.points[0]
+            #Adjust speeds
+            times = []
+            for pt in trajectory.trajectory.points:
+                pt.time_from_start = rospy.Duration(pt.time_from_start.to_sec()/self.speed_factor)
+                if pt.velocities is not None:
+                    pt.velocities = (np.array(pt.velocities)*self.speed_factor).tolist()
+                if pt.accelerations is not None:
+                    pt.accelerations = (np.array(pt.accelerations)*self.speed_factor).tolist()
+                
+                times.append(pt.time_from_start.to_sec())
+
+        elif isinstance(trajectory, JointTrajectory):
             first_pt = trajectory.points[0]
+
             #Adjust speeds
             for pt in trajectory.points:
-                pt.time_from_start = pt.time_from_start/self.speed_factor
-                if pt.twist is not None:
-                    pt.twist.linear.x = pt.twist.linear.x*self.speed_factor
-                    pt.twist.linear.y = pt.twist.linear.y*self.speed_factor
-                    pt.twist.linear.z = pt.twist.linear.z*self.speed_factor
-                    pt.twist.angular.x = pt.twist.angular.x*self.speed_factor
-                    pt.twist.angular.y = pt.twist.angular.y*self.speed_factor
-                    pt.twist.angular.z = pt.twist.angular.z*self.speed_factor
-
+                pt.time_from_start = rospy.Duration(pt.time_from_start.to_sec()/self.speed_factor)
+                if pt.velocities is not None:
+                    pt.velocities = (np.array(pt.velocities)*self.speed_factor).tolist()
+                if pt.accelerations is not None:
+                    pt.accelerations = (np.array(pt.accelerations)*self.speed_factor).tolist()
         else:
             first_pt = trajectory[0]
+
             #Adjust speeds
             for pt in trajectory:
                 pt['time'] = pt['time']/self.speed_factor
-
-                if 'linear_velocity' in pt.keys() and 'angular_velocity' in pt.keys():
-                    pt['linear_velocity'] = (np.array(pt['linear_velocity'])*self.speed_factor).tolist() 
-                    pt['angular_velocity'] = (np.array(pt['angular_velocity'])*self.speed_factor).tolist()
+                if 'velocities' in pt.keys():
+                    pt['velocities'] = (np.array(pt['velocities'])*self.speed_factor).tolist()
+                if 'accelerations' in pt.keys():
+                    pt['accelerations'] = (np.array(pt['accelerations'])*self.speed_factor).tolist()
 
         # Go to the beginning of the trajectory
         if perform_init:
